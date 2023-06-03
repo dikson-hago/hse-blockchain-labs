12cd3ab9bed1219c69272cf41883608f18e5854c
user1 <alexjones20191@gmail.com>
2023-06-03 19:08:50 +0100

    changed filter

--- deployments/token.js
+++ deployments/token.js
@@ -4,7 +4,7 @@ const ContractArtifact = require('../artifacts/contracts/Token.sol/Token.json')
 
 async function main() {
     const [signer] = await ethers.getSigners()
-    const contractAddr = '0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690'
+    const contractAddr = '0x85B78cF24C0D540AF9a87931C939D2586Ccaa172'
 
     const contractInstance = new ethers.Contract(
         contractAddr,
@@ -13,16 +13,15 @@ async function main() {
     )
 
     const number = ethers.utils.parseEther("10")
-    await contractInstance.addStructure(number, "test", false, {gasLimit: 100})
+    await contractInstance.addStructure(number, "test", false, {gasLimit: 900000})
     console.log("add structure")
-    await contractInstance.deleteStructure({gasLimit: 100})
+    await contractInstance.deleteStructure({gasLimit: 900000})
     console.log("delete structure")
 
     console.log("start filter")
     const filter = {
-        gasLimit: 10,
-        address: '0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690',
-        topics: ['0x584d5f20f540ffb3bd3fe29955568b7e1f429a7a4652ab2763ed5bccfc7054ca']
+        gasLimit: 900000,
+        address: '0x85B78cF24C0D540AF9a87931C939D2586Ccaa172',
     }
 
     await contractInstance.queryFilter(filter).then((events) => {
