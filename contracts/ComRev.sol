fd496104479b214a8bbb5137099c8b6bbe26b9a6
user1 <alexjones20191@gmail.com>
2023-06-03 13:49:40 +0100

    add rock-scessors-paper game

---
+++ contracts/ComRev.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0 <0.9.0;
+
+contract ComRev {
+    uint[] public candidates = 
+    [
+        1, // rock
+        2, // paper
+        3 // scissors
+    ];
+
+    mapping(address => bytes32) public commits;
+    mapping(uint => uint) public votes;
+    bool votingStopped;
+
+    function commitVote(bytes32 _hashedVote) external {
+        require(!votingStopped);
+        require(commits[msg.sender] == bytes32(0));
+
+        commits[msg.sender] = _hashedVote;
+    }
+
+    // open, who wins
+    function revealVoting(uint _candidate, bytes32 _secret) external {
+        require(votingStopped);
+
+        bytes32 commit = keccak256(abi.encodePacked(_candidate, _secret, msg.sender));
+
+        require(commit == commits[msg.sender]);
+
+        delete commits[msg.sender];
+
+        votes[_candidate]++;
+    }
+
+    function stopVoting() external {
+        require(!votingStopped);
+
+        votingStopped = true;
+    }
+}
